be758e33b697d177b6a05cababf093dd1a2ac9a0
Jorge Diz <jorge.a.diz@gmail.com>
2015-09-02 08:45:05 -0300

    update

--- src/test/java/glia/myserenitydemo/DefinitionTestSuite.java
+++
@@ -1,9 +0,0 @@
-package glia.myserenitydemo;
-
-import cucumber.api.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
-
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features="src/test/resources/features/consult_dictionary/LookupADefinition.feature")
-public class DefinitionTestSuite {}
